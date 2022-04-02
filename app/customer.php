6f649165cbf1d2b12ff2fa3506082f0115d24a42
marwan <marwanabdalla1999@gmail..com>
2022-04-02 19:49:56 +0200

    ...

--- app/customer.php
+++ app/customer.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class customer extends Model
 {
-    protected $table='user';
+    protected $table='users';
     protected $fillable=['name','phone','token','created_at','updated_at','otp','verifyed'];
 }

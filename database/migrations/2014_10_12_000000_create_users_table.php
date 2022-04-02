ee45bb33313379d764f09d22a1eb0387570401af
marwan <marwanabdalla1999@gmail..com>
2022-04-02 18:05:53 +0200

    ...

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->primary('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('phone');
+            $table->string('token');
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+            $table->string('otp');
+            $table->string('verifyed');
+
         });
     }
 

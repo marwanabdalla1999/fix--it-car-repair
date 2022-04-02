00e2f42382b943f945b96544465abab7d2560cb5
marwan <marwanabdalla1999@gmail..com>
2022-04-02 20:39:00 +0200

    ...

--- app/Http/Controllers/customer1.php
+++ app/Http/Controllers/customer1.php
@@ -32,39 +32,49 @@ class customer1 extends Controller
         }
     }
 
+function send_otp_and_register_user(Request $request){
+
+    $sid = "AC8c9c14a5e759d4f2c334c5db8e47f100"; // Your Account SID from www.twilio.com/console
+    $token = "185eb72ee597fc68f0c92f427afc9d32"; // Your Auth Token from www.twilio.com/console
+    $otp = random_int(100000, 999999);
+    $client = new Client($sid, $token);
+    $message = $client->messages->create(
+        '+'.$request->phone, // Text this number
+        [
+            'from' => '+12762849947', // From a valid Twilio number
+            'body' => 'Hello your otp code is '.$otp.' !'
+        ]
+    );
+
+    $data= customer::Create([
+        'name'=> $request->name,
+        'phone'=> $request->phone,
+        'token'=> Str::random(50),
+        'otp' =>$otp,
+        'verifyed'=>'false'
+    ]);
+
+    return 'otp has been sent';
+}
+
 
     function register(Request $request){
-        // Send an SMS using Twilio's REST API and PHP
-        $sid = "AC8c9c14a5e759d4f2c334c5db8e47f100"; // Your Account SID from www.twilio.com/console
-        $token = "185eb72ee597fc68f0c92f427afc9d32"; // Your Auth Token from www.twilio.com/console
-        $otp = random_int(100000, 999999);
+
 
         $user_id = customer::where('phone', $request->phone)->first();
-        if ($request->phone == $user_id->phone){
+        if ($user_id==null){
 
+            return send_otp_and_register_user($request);
 
+        }
+       else if ($request->phone == $user_id->phone){
 
             return 'this number is used before';
         }
         else{
-        $client = new Client($sid, $token);
-        $message = $client->messages->create(
-            '+'.$request->phone, // Text this number
-            [
-                'from' => '+12762849947', // From a valid Twilio number
-                'body' => 'Hello your otp code is '.$otp.' !'
-            ]
-        );
+            return send_otp_and_register_user($request);
 
-        $data= customer::Create([
-            'name'=> $request->name,
-                'phone'=> $request->phone,
-                'token'=> Str::random(50),
-                'otp' =>$otp,
-                'verifyed'=>'false'
-                ]);
 
-        return 'otp has been sent';
         }
 
     }

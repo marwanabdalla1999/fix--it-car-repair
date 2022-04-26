f169aafc499105f082a90e54be3fd6deb7ad3f4b
marwan <marwanabdalla1999@gmail..com>
2022-04-26 22:04:24 +0200

    ...

--- app/Http/Controllers/Order.php
+++ app/Http/Controllers/Order.php
@@ -76,7 +76,7 @@ class Order extends Controller
 
     //    if ($response=='login'){
 
-            $offers=tech_offer::where('order_id',$request->order_id)->frist();
+            $offers=tech_offer::where('order_id',$request->order_id)->first();
                 if ($offers!=null){
             return $offers;
                 //}

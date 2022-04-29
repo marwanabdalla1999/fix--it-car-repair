b352db32610b23e0f6ba2e4622a74f127ac734e9
marwan <marwanabdalla1999@gmail..com>
2022-04-29 22:41:13 +0200

    ...

--- app/provider_data.php
+++ app/provider_data.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class provider_data extends Model
 {
     protected $table='Technicans';
-    protected $fillable=['name','phone','token','created_at','photo'];
+    protected $fillable=['name','phone','token','updated_at','created_at','photo'];
 }

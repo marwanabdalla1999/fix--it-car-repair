cdfb5d85c8a6ac62fb81cfde028dcf7b9d309884
marwan <marwanabdalla1999@gmail..com>
2022-04-29 23:03:40 +0200

    ...

--- app/Http/Controllers/technican.php
+++ app/Http/Controllers/technican.php
@@ -24,7 +24,7 @@ class technican extends Controller
             'name' => $request->name,
             'phone' => $request->phone,
             'token' => Str::random(50),
-            'photo' => $request->photo
+            'photo' => $request->file('photo')
 
         ]);
 

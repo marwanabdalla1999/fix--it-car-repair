21c8c32f14c37959957f4411425f27bbebaad0fc
marwan <marwanabdalla1999@gmail..com>
2022-04-29 21:48:31 +0200

    ...

--- resources/views/register_provider.blade.php
+++ resources/views/register_provider.blade.php
@@ -48,40 +48,14 @@
 
                 <!-- Text input-->
 
-                <div class="form-group">
-                    <label class="col-md-4 control-label">Username</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input  name="user_name" placeholder="Username" class="form-control"  type="text">
-                        </div>
-                    </div>
-                </div>
+
 
                 <!-- Text input-->
 
-                <div class="form-group">
-                    <label class="col-md-4 control-label" >Password</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input name="user_password" placeholder="Password" class="form-control"  type="password">
-                        </div>
-                    </div>
-                </div>
 
                 <!-- Text input-->
 
-                <div class="form-group">
-                    <label class="col-md-4 control-label" >Confirm Password</label>
-                    <div class="col-md-4 inputGroupContainer">
-                        <div class="input-group">
-                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                            <input name="confirm_password" placeholder="Confirm Password" class="form-control"  type="password">
-                        </div>
-                    </div>
-                </div>
-
+     
                 <!-- Text input-->
                 <div class="form-group">
                     <label class="col-md-4 control-label">photo</label>

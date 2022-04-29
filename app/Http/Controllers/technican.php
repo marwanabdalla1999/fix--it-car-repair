db3984f6339d5094f77a1b18edece37679dcb053
marwan <marwanabdalla1999@gmail..com>
2022-04-29 21:02:27 +0200

    ...

---
+++ app/Http/Controllers/technican.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\provider_data;
+use Illuminate\Http\Request;
+
+class technican extends Controller
+{
+    function add_new_provider($request){
+        $input=$request->all();
+            if ($request->hasFile('image')){
+                $destination='public/images/provier_photos';
+                $image=$request->file('image');
+                $image_name=$image->getClientOriginalName();
+                $path=$request->file('image')->storeAs($destination,$image_name);
+                    $input['image']=$image_name;
+            }
+        $data = provider_data::Create([
+
+            'name' => $request->name,
+            'phone' => $request->phone,
+            'token' => Str::random(50),
+            'photo' => $input->image,
+
+        ]);
+
+
+    }
+}

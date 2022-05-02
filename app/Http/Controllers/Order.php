cf0836035aeeb80f458f3b273e7f0075663f6d6e
marwan <marwanabdalla1999@gmail..com>
2022-05-02 18:34:51 +0200

    ...

--- app/Http/Controllers/Order.php
+++ app/Http/Controllers/Order.php
@@ -122,14 +122,15 @@ class Order extends Controller
 
             foreach ($requests as $req){
 
-               $order_data =order_model::where('id',(int)$req->order_id)->frist();
+               order_model::where('id',(int)$req->order_id)->frist();
 
                $temp= response()->json([
                    'id' => $req->id,
                    'issue' => $req->issue,
-                   'address' => $order_data->address,
-                   'payment_way' => $order_data->payment_way,
-                   'location_lat_lng' => $order_data->location_lat_lng]);
+                 //  'address' => $order_data->address,
+                //   'payment_way' => $order_data->payment_way,
+                //   'location_lat_lng' => $order_data->location_lat_lng
+                   ]);
 
                 $requests_final[]=$temp;
             }

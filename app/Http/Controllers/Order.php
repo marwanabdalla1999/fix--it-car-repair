044e848a41a126f35a97f25eabb6ede48363948c
marwan <marwanabdalla1999@gmail..com>
2022-05-05 00:53:06 +0200

    ...

--- app/Http/Controllers/Order.php
+++ app/Http/Controllers/Order.php
@@ -121,8 +121,8 @@ class Order extends Controller
                 }
                 $finalrequests = array_diff($requests, $removed_requests);
 
-        if ($finalrequests!=null){
-            return $finalrequests;
+        if ($requests!=null){
+            return $requests;
 
 
         }

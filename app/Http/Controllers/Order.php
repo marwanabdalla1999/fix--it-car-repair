467bfcc0841415de15771666bf4f7f3166600df8
marwan <marwanabdalla1999@gmail..com>
2022-05-18 20:38:34 +0200

    ...

--- app/Http/Controllers/Order.php
+++ app/Http/Controllers/Order.php
@@ -302,6 +302,7 @@ class Order extends Controller
                            'name' => $provider_data->name,
                             'phone' => $provider_data->phone,
                             'rate' => $provider_data->rate,
+                           'state'=>$user_order->state
 
                         ]
                     );

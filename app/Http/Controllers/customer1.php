52f49f84d13c2b088a0fc5cd36e2d8af3a48169c
marwan <marwanabdalla1999@gmail..com>
2022-05-22 22:34:16 +0200

    ...

--- app/Http/Controllers/customer1.php
+++ app/Http/Controllers/customer1.php
@@ -123,7 +123,7 @@ function session(Request $request)
 {
     $user_id = customer::where([['id', '=', $request->id], ['token', '=', $request->token]])->first();
     if ($user_id) {
-        $user_id->device_token=$request->device->device_token;
+        $user_id->device_token=$request->device_token;
         $user_id->save();
         $hasOrder = order_model::where([['user_id', '=', $request->id],
             ['state', '=', 'in progress']])->orWhere([['user_id', '=', $request->id],

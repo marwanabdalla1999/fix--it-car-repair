d89204c2a74af5f0651fcb6c8e795ca2f709349e
marwan <marwanabdalla1999@gmail..com>
2022-06-26 21:09:23 +0200

    ... 4

--- app/user_cards.php
+++ app/user_cards.php
@@ -8,6 +8,6 @@ class user_cards extends Model
 {
 
     protected $table='user_cards';
-    protected $fillable=['user_id','token','mask_pan','card_id'];
+    protected $fillable=['user_id','token','mask_pan','type'];
 
 }

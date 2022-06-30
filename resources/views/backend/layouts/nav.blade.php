aba8aff0cec362bf86e3ee490394f2cc59327512
marwan <marwanabdalla1999@gmail..com>
2022-06-30 23:12:07 +0200

    ... 4

--- resources/views/backend/layouts/nav.blade.php
+++ resources/views/backend/layouts/nav.blade.php
@@ -13,7 +13,6 @@
 
             <br/>
 
-            <hr style="background-color:black;"/>
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
                 <div class="menu_section">
                     <h3>General</h3>
